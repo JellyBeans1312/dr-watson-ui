d4cd5b20ec682a57e2c64c868ccdd7713244242c
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-18 15:41:43 -0600

    fix issue where conversation did not persist

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,9 +1,9 @@
 export const messages = (state = [], action) => {
   switch(action.type) {
     case 'ADD_MESSAGES' :
-      return [{ message: action.messages, isUser: action.user}]
+      return [...state, { message: action.messages, isUser: action.user}]
     case 'CLEAR_MESSAGES' :
-      return state
+      return []
     default : 
       return state
   }
